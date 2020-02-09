c914dd77f12317dd411f3e34ac348c49928c651b
Zofia Drabek <zosia.drabek@gmail.com>
2020-02-09 16:08:44 +0100

    Add Event struct.

---
+++ Events/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  Events
+//
+//  Created by Zofia Drabek on 09/02/2020.
+//  Copyright © 2020 Zofia Drabek. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    var title: String
+    var date: Date
+}
